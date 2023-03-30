cb0185409849ab9eec2d130faaa8f94d1c5858a2
Luis Francisco Morales Maldonado <luiste49@gmail.com>
2023-03-30 11:58:00 +0200

    all mem*.c added

---
+++ ft_memcmp.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lumorale <lumorale@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/06 11:16:48 by lumorale          #+#    #+#             */
+/*   Updated: 2022/12/09 13:14:21 by lumorale         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+	size_t			i;
+	unsigned char	*ss1;
+	unsigned char	*ss2;
+
+	i = 0;
+	ss1 = (unsigned char *)s1;
+	ss2 = (unsigned char *)s2;
+	while (i < n)
+	{
+		if (ss1[i] != ss2[i])
+			return (ss1[i] - ss2[i]);
+				i++;
+	}
+	return (0);
+}
---
+++ ft_memmove.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lumorale <lumorale@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/01 11:17:20 by lumorale          #+#    #+#             */
+/*   Updated: 2022/12/13 10:55:25 by lumorale         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <string.h>
+#include "libft.h"
+
+void	*ft_memmove(void *dest, const void *src, size_t n)
+{
+	if (!dest && !src)
+		return (0);
+	if (dest < src)
+		ft_memcpy(dest, src, n);
+	else
+		while (n--)
+			((unsigned char *)dest)[n] = ((unsigned char *)src)[n];
+	return (dest);
+}
